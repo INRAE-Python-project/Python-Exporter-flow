f5ec9d2f8f919114fffd2a14ddb990f19bdc75b6
gaeta <gaetan.gonfiantini@gmail.com>
2024-03-27 22:32:18 +0100

    Le script init.sh est correcte

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Fonction pour vérifier si Python 3.11 est installé
 check_python_version() {
     PYTHON_VERSION=$(python3.11 --version 2>&1)
@@ -9,33 +10,44 @@ check_python_version() {
         install_python
     fi
 }
-
 # Fonction pour installer Python 3.11
 install_python() {
     sudo apt update
     sudo apt install -y python3.11 python3.11-venv python3.11-dev
 }
-
-# Fonction pour installer pip pour Python 3.11
+# Fonction pour vérifier et installer pip pour Python 3.11 si nécessaire
 install_pip() {
-    echo "Installation de pip pour Python 3.11..."
-    wget https://bootstrap.pypa.io/get-pip.py
-    sudo python3.11 get-pip.py --break-system-packages
+    if ! python3.11 -m pip --version &> /dev/null; then
+        echo "pip pour Python 3.11 n'est pas installé. Installation..."
+        wget -O get-pip.py https://bootstrap.pypa.io/get-pip.py
+        sudo python3.11 get-pip.py --break-system-packages
+        rm get-pip.py
+    else
+        echo "pip pour Python 3.11 est déjà installé."
+    fi
 }
 
-# Fonction pour installer les dépendances Python
+# Fonction pour installer les dépendances Python si nécessaire
 install_dependencies() {
-    echo "Installation de Scapy avec apt..."
-    sudo apt install -y python3-scapy
+    if ! python3.11 -c "import scapy" &> /dev/null; then
+        echo "Installation de Scapy avec apt..."
+        sudo apt install -y python3-scapy
+    else
+        echo "Scapy est déjà installé."
+    fi
 
-    echo "Installation de la bibliothèque Keyboard avec pip..."
-    sudo python3.11 -m pip install keyboard --break-system-packages
+    if ! python3.11 -c "import keyboard" &> /dev/null; then
+        echo "Installation de la bibliothèque Keyboard avec pip..."
+        sudo python3.11 -m pip install keyboard --break-system-packages
+    else
+        echo "La bibliothèque Keyboard est déjà installée."
+    fi
 }
 
 # Fonction pour exécuter le script Python
 run_python_script() {
     echo "Lancement du script Python..."
-    sudo python3.11 /home/gonfiant/Bureau/Python-Exporter-flow/exporter-flow.py
+    sudo python3.11 ./exporter-flow.py
 }
 
 # Vérification de la version de Python
@@ -44,7 +56,7 @@ check_python_version
 # Installation de pip pour Python 3.11 si nécessaire
 install_pip
 
-# Installation des dépendances Python
+# Installation des dépendances Python si nécessaire
 install_dependencies
 
 # Exécution du script Python
